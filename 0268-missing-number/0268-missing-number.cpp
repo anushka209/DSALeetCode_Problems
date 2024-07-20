2758eeb5dd41cd25448235834c48bde7d37c12de
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-07-20 17:05:13 +0530

    Time: 24 ms (10.32%), Space: 21.5 MB (5.23%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        if(nums[n-1]!=n) return n;
+        for(int i=0;i<n;i++)
+        {
+            if(i!=nums[i])
+                return i;
+        }
+        return 0;
+    }
+};
