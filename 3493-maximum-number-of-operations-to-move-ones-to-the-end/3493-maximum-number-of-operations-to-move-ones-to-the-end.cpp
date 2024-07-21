04b362fc25e91f6221de96a3529e0e49dfea48d5
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-07-21 12:03:09 +0530

    Time: 25 ms (100%), Space: 14.4 MB (33.33%) - LeetHub

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int maxOperations(string s) {
-       int n = s.length(), cnt = 0, ans = 0;
-        for(int i = 0; i < n; i++){
-            if(s[i] == '0'){
-                ans += cnt;
-                while(i < n && s[i] != '1'){
-                    i++;
-                }
-            }
-            cnt++;
+      int n=s.size();
+      int count=0,ans=0;
+      for(int i=0;i<n;i++)
+      {
+        if(s[i]=='0')         //in case of 0
+        {
+            ans+=count;
+            while(i<n && s[i]!='1')   //for consecutive 0s
+                i++;
         }
-        return ans;
+         count++;    //in case of 1
+      }
+      return ans;
     }
 };
