e0fab86b17a11aa9b1e74097a859a0070a439c3e
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-07-21 15:15:11 +0530

    Time: 100 ms (61.97%), Space: 61.5 MB (72.68%) - LeetHub

--- 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.cpp
+++ 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.cpp
@@ -1,51 +1,71 @@
 class Solution {
 public:
-    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
-         vector<vector<int>> rowGraph(k + 1), colGraph(k + 1);
-        for (auto& rc : rowConditions) {
-            rowGraph[rc[0]].push_back(rc[1]);
+      vector<int> topoSort(vector<vector<int>>& edges, int n) {
+        unordered_map<int, vector<int>> adj;
+        stack<int> st;
+        vector<int> order;
+        //0 : not visited;
+        //1 : visiting (currently in stack via some other DFS)
+        //2 : visited
+        vector<int> visited(n + 1, 0);
+        bool hasCycle = false;
+
+        for (vector<int>& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
         }
-        for (auto& cc : colConditions) {
-            colGraph[cc[0]].push_back(cc[1]);
+
+        for (int i = 1; i <= n; i++) {
+            if (visited[i] == 0) {
+                dfs(i, adj, visited, st, hasCycle);
+                if (hasCycle) 
+                    return {}; //no ordering possible
+            }
         }
         
-        vector<int> rowOrder = topoSort(rowGraph, k);
-        vector<int> colOrder = topoSort(colGraph, k);
-        
-        if (rowOrder.empty() || colOrder.empty()) return {};
-        
-        vector<vector<int>> result(k, vector<int>(k, 0));
-        unordered_map<int, int> rowMap, colMap;
-        for (int i = 0; i < k; ++i) {
-            rowMap[rowOrder[i]] = i;
-            colMap[colOrder[i]] = i;
+        while(!st.empty()) {
+            order.push_back(st.top());
+            st.pop();
         }
+        return order;
+    }
+    
+    void dfs(int node, unordered_map<int, vector<int>>& adj, vector<int>& visited,
+             stack<int>& st, bool& hasCycle) {
         
-        for (int i = 1; i <= k; ++i) {
-            result[rowMap[i]][colMap[i]] = i;
+        visited[node] = 1;  // Mark node as visiting
+        //First, visit node's children
+        for (int& nbr : adj[node]) {
+            if (visited[nbr] == 0) {
+                dfs(nbr, adj, visited, st, hasCycle);
+            } else if (visited[nbr] == 1) {
+                // Cycle detected
+                hasCycle = true;
+                return;
+            }
         }
-        
-        return result;
+
+        visited[node] = 2; //visited
+        st.push(node); //Now node can be added
     }
 
-       vector<int> topoSort(vector<vector<int>>& graph, int k) {
-        vector<int> inDegree(k + 1, 0), order;
-        queue<int> q;
-        for (int i = 1; i <= k; ++i) {
-            for (int j : graph[i]) {
-                ++inDegree[j];
+    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
+       vector<int> orderRows    = topoSort(rowConditions, k);
+        vector<int> orderColumns = topoSort(colConditions, k);
+
+        // We might have found cycle. That's why topo order is empty
+        if (orderRows.empty() or orderColumns.empty())
+            return {};
+
+        vector<vector<int>> matrix(k, vector<int>(k, 0));
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++) {
+                if (orderRows[i] == orderColumns[j]) {
+                    matrix[i][j] = orderRows[i];
+                }
             }
         }
-        for (int i = 1; i <= k; ++i) {
-            if (inDegree[i] == 0) q.push(i);
-        }
-        while (!q.empty()) {
-            int node = q.front(); q.pop();
-            order.push_back(node);
-            for (int adj : graph[node]) {
-                if (--inDegree[adj] == 0) q.push(adj);
-            }
-        }
-        return order.size() == k ? order : vector<int>();
+        return matrix;
     }
 };
