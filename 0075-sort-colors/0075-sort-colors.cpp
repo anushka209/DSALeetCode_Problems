626228006ec95c7ade0c731791e8b605c0f37ec8
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-07-23 00:33:24 +0530

    Time: 0 ms (100%), Space: 10 MB (9.59%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int i=0,j=0,k=n-1;
+        while(j<=k)
+        {
+            switch(nums[j])
+            {
+                case 0: swap(nums[i++],nums[j++]);
+                break;
+                case 1: j++;
+                break;
+                case 2:swap(nums[j],nums[k--]);
+                break;
+            }
+        }
+    }
+};
