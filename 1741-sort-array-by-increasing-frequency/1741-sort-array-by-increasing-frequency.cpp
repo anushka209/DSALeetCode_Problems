dd95bbdb6e1044df2c41ee9fce312d669edb7e73
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-07-23 07:58:24 +0530

    Time: 4 ms (73.1%), Space: 15.1 MB (25.59%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
+    bool comp(int a, int b, unordered_map<int, int>& mpp) {
+    if (mpp.at(a) != mpp.at(b)) {
+        return mpp.at(a) < mpp.at(b);    //increasing order based on frequency
+    } 
+    else 
+        return a > b;       // decreasing order by the number
+}
+
     vector<int> frequencySort(vector<int>& nums) {
         unordered_map<int,int>mpp;      //store number & count
         for(int i=0;i<nums.size();i++)
         {
             mpp[nums[i]]++;
         }
+        // sort(nums.begin(), nums.end(), [&](int a, int b) {
+        //     if (mpp[a] != mpp[b]) {
+        //         // if freq of numbers are not equal then return in increasing order based on freq.
+        //         return mpp[a] < mpp[b];
+        //     } else {
+        //         // otherwise sort them in decreasing order based on number in nums.
+        //         return a > b;
+        //     }
+        // });
         sort(nums.begin(), nums.end(), [&](int a, int b) {
-            if (mpp[a] != mpp[b]) {
-                // if freq of numbers are not equal then return in increasing
-                // order based on freq.
-                return mpp[a] < mpp[b];
-            } else {
-                // otherwise sort them in decreasing order based on number in nums.
-                return a > b;
-            }
+        return comp(a,b,mpp);
         });
         return nums;
     }
