952d625fb45553c9f980ef1fa441b01996d1ca82
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-07-24 23:35:22 +0530

    Time: 109 ms (29.77%), Space: 53.9 MB (5.69%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int count=0;
+        // int count=0;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     int sum=0;
+        //     for(int j=i;j<nums.size();j++)
+        //     {
+        //         sum+=nums[j];
+        //         if(sum==k)
+        //             count++;
+        //     }
+        // }
+        // return count;
+
+        map<int,int>mpp;    //preSum & count        //using map approach
+        int count=0,preSum=0;
+        mpp[0]=1;       //most imp. case
         for(int i=0;i<nums.size();i++)
         {
-            int sum=0;
-            for(int j=i;j<nums.size();j++)
-            {
-                sum+=nums[j];
-                if(sum==k)
-                    count++;
-            }
+            preSum+=nums[i];             // add current element to prefix Sum:
+            int remove=preSum-k;         // Calculate x-k:
+            count+=mpp[remove];       // Add the number of subarrays to be removed:
+            mpp[preSum]+=1;           // Update the count of prefix sum in the map.
         }
         return count;
     }
