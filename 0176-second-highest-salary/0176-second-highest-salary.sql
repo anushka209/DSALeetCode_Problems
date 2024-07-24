62fc63e6d22b5997384db24dc00fe0169ee5dd8f
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-07-24 11:14:57 +0530

    Time: 686 ms (7.37%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,10 @@
 # Write your MySQL query statement below
 
 
-select max(e1.salary) as SecondHighestSalary from Employee e1 join Employee e2 on e1.salary < e2.salary;
+-- select max(e1.salary) as SecondHighestSalary from Employee e1 join Employee e2 on e1.salary < e2.salary;
+
+select (select distinct salary from Employee order by salary desc LIMIT 1 OFFSET 1) as SecondHighestSalary;
+
+-- Extract distinct salaries: Using the DISTINCT keyword ensures we do not have duplicate salary values.
+-- Sort salaries in descending order: Using ORDER BY salary DESC arranges the salaries from highest to lowest.
+-- Offset and limit: Using OFFSET 1 skips the highest salary and LIMIT 1 fetches the second highest salary.
