78955a2d925c12be03eade1569df7592c5268e4c
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-08-02 12:41:17 +0530

    Time: 6 ms (31.31%), Space: 11.5 MB (29.23%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-       int maxi=INT_MIN;
-       int ans=0;
-       for(int i=0;i<nums.size();i++)
-       {
-          if(nums[i]>maxi)
-          {
-            maxi=nums[i];
-            ans=i;
-          }
-       }
-       return ans;
+    //    int maxi=INT_MIN;
+    //    int ans=0;
+    //    for(int i=0;i<nums.size();i++)
+    //    {
+    //       if(nums[i]>maxi)
+    //       {
+    //         maxi=nums[i];
+    //         ans=i;
+    //       }
+    //    }
+    //    return ans;
+
+     int n=nums.size();
+     if(n==1) return 0;
+     if(nums[0]>nums[1]) return 0;
+     if(nums[n-1]>nums[n-2]) return n-1;
+     int s=1,e=n-2;
+     while(s<=e)
+     {
+        int mid=s+(e-s)/2;
+        if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1])
+            return mid;
+        if(nums[mid]>nums[mid-1])    // we are in left
+            s=mid+1;
+        else
+            e=mid-1;       // we are in right
+     }
+     return -1;
     }
 };
 
-    // int n=arr.size();
-    // // base case
-    // if(n==1) return 0;
-    // if(arr[0]>arr[1]) return 0;
-    // if(arr[n-1]>arr[n-2]) return n-1;
-
-    // int s=1,e=n-2;
-    // while(s<=e)
-    // {
-    //     int mid=s+(e-s)/2;
-    //     if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
-    //     return mid;
-
-    //     if(arr[mid]>arr[mid-1])
-    //     {
-    //         s=mid+1;
-    //     }
-    //     else
-    //     {
-    //         e=mid-1;
-    //     }
-    // }
-    // return -1;
+  
