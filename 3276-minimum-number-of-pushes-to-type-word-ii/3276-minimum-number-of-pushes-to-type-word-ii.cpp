2aef903063c0b140b5d3b0b0acf5961ba28348c8
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-08-06 10:23:25 +0530

    Time: 68 ms (79.47%), Space: 25.1 MB (57.94%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        vector<int> count(26);
-        for(char& ch:word)
-        {
-            ++count[ch-'a'];
-        }
-        sort(count.rbegin(),count.rend());
-        int ans=0;
-        for(int i=0;i<26;++i)
-        {
-            ans+=(i/8+1)*count[i];
-        }
-        return ans;
+      vector<int>v(26,0);
+      int ans=0;
+      for(char &ch:word)
+      {
+        v[ch-'a']++;
+      }
+      sort(v.begin(),v.end(),greater<int>());   //descending order
+      for(int i=0;i<26;i++)
+      {
+         int freq=v[i];
+         int press=i/8 +1;
+         ans+=freq*press;
+      }
+      return ans;
     }
 };
