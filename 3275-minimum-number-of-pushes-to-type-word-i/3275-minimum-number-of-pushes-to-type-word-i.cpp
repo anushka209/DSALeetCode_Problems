9ab5ddba65fc9890efd6173763c940222e0130ec
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-08-06 10:04:29 +0530

    Time: 3 ms (39.41%), Space: 9.6 MB (9.12%) - LeetHub

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int ans=0;
+        unordered_map<int,int>mpp;
+
+        int assign_key=2;
+        for(auto& ch:word)
+        {
+            if(assign_key>9)
+                assign_key=2;
+
+            mpp[assign_key]++;
+            ans+=mpp[assign_key];
+            assign_key++;
+        }
+        return ans;
+    }
+};
