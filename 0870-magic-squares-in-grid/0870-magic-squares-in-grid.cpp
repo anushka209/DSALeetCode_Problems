017ce061eafd63691fb8049496fc4d3a45aa4ef7
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-08-09 10:59:57 +0530

    Time: 4 ms (38.66%), Space: 11.3 MB (47.9%) - LeetHub

--- 0870-magic-squares-in-grid/0870-magic-squares-in-grid.cpp
+++ 0870-magic-squares-in-grid/0870-magic-squares-in-grid.cpp
@@ -28,7 +28,7 @@ private:
             }
         }
         sort(nums.begin(),nums.end());
-        for(int k=0;k<9;k++)
+        for(int k=0;k<9;k++)         //check if all numbers are present or not
         {
             if(nums[k]!=k+1)
                 return false;
@@ -50,10 +50,10 @@ private:
         }
 
         // Check diagonals
-        if (grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2] != sum) {
+        if (grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2] != sum) {   //left diagonal
             return false;
         }
-        if (grid[r][c + 2] + grid[r + 1][c + 1] + grid[r + 2][c] != sum) {
+        if (grid[r][c + 2] + grid[r + 1][c + 1] + grid[r + 2][c] != sum) {    //right diagonal
             return false;
         }
 
