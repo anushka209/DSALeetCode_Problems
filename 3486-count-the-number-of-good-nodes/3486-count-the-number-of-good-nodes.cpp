3592158eb63a0709ae87b2a2746db05d12a45768
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-08-11 16:48:18 +0530

    Time: 1299 ms (100%), Space: 449.2 MB (50%) - LeetHub

--- 3486-count-the-number-of-good-nodes/3486-count-the-number-of-good-nodes.cpp
+++ 3486-count-the-number-of-good-nodes/3486-count-the-number-of-good-nodes.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> adj;
-    vector<int> subtreeSize;
-    int goodNodes = 0;
+    int goodNodes=0;
 
-    int dfs(int node, int parent) {
-        int totalNodes=1;
-        int childSize = -1;
-        bool isGood = true;
-    
-        for (int neighbor : adj[node]) {
-            if (neighbor == parent) continue;
-        
-            int currentSize = dfs(neighbor, node);
-        
-            if (childSize == -1) {
-                childSize = currentSize;
-            } else if (childSize != currentSize) {
-                isGood = false;
-            }  
-            totalNodes += currentSize;
+    int dfs(int node, int parent,vector<vector<int>> &adj) {
+        int subtreeSize=1;
+        unordered_set<int>s;
+
+        for(int it : adj[node])
+        {
+            if(it==parent) continue;
+            int currentSize = dfs(it,node,adj);
+            subtreeSize += currentSize;
+            s.insert(currentSize);
         }
-    
-        if (isGood) goodNodes++;
-        return totalNodes;
+        if(s.size()<=1)  //if <1 then leaf node or if =1 then only 1 node
+            goodNodes++;
+        return subtreeSize;
     }
     
     int countGoodNodes(vector<vector<int>>& edges) {
-        int n = edges.size() + 1;
-        adj.resize(n);
-        subtreeSize.resize(n);
-    
-        for (auto& edge : edges) {
+        int n=edges.size()+1;
+        vector<vector<int>> adj(n);
+
+        for(auto& edge : edges) {
             adj[edge[0]].push_back(edge[1]);
             adj[edge[1]].push_back(edge[0]);
         }
-        dfs(0, -1);
+        dfs(0, -1,adj);
         return goodNodes;
     }
 };
