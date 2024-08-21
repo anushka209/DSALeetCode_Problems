dee53b94531056102e00afb9bc199546a1d470a2
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-08-21 12:37:25 +0530

    Time: 8 ms (68.69%), Space: 12.8 MB (56.38%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,24 +1,28 @@
- #include<bits/stdc++.h>
+#include<bits/stdc++.h>
 class Solution {
 public:
     int romanToInt(string s) {
-       map<char,int>mpp;
-       mpp.insert({'I',1});
-       mpp.insert({'V',5});
-       mpp.insert({'X',10});
-       mpp.insert({'L',50});
-       mpp.insert({'C',100});
-       mpp.insert({'D',500});
-       mpp.insert({'M',1000});
+        map<char,int>m;
+        m.insert({'I',1});
+        m.insert({'V',5});
+        m.insert({'X',10});
+        m.insert({'L',50});
+        m.insert({'C',100});
+        m.insert({'D',500});
+        m.insert({'M',1000});
 
-       int ans=mpp[s[s.size()-1]];
-       for(int i=s.size()-2;i>=0;i--)
-       {
-        if(mpp[s[i]]<mpp[s[i+1]])
-            ans-=mpp[s[i]];
-        else
-            ans+=mpp[s[i]];
-       }
-       return ans;
+        int res= m[s[s.size()-1]];
+        for(int i=s.size()-2;i>=0;i--)
+        {
+            if(m[s[i]]<m[s[i+1]])
+            {
+                res -= m[s[i]];
+            }
+            else
+            {
+                res += m[s[i]];
+            }
+        }
+    return res;
     }
 };
