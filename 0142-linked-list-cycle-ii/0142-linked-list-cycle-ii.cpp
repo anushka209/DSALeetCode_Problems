6c7ee02950137a373c981144925f7e6517b9ccd9
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-08-26 17:58:47 +0530

    Time: 10 ms (29.33%), Space: 10.4 MB (61.96%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow=head;                  //tortoise and hare algorithm
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
+            {
+                slow=head;
+                while(slow!=fast)
+                {
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                return slow;   // or return fast any of the two becoz they are equal & it is the starting point
+            }
+        }
+        return NULL;
+    }
+};
