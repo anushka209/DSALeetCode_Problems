aeb59ab040e026da7318757db10654e19a38a044
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-08-26 19:09:53 +0530

    Time: 208 ms (6.69%), Space: 122.9 MB (45.34%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ #include<stack>
+class Solution {
+public:
+
+ListNode* reverse(ListNode* head)      //recursive reverse function
+{
+    if(head==NULL || head->next==NULL)
+        return head;
+    ListNode* newHead=reverse(head->next);
+    ListNode* front=head->next;
+    front->next=head;
+    head->next=nullptr;
+    return newHead;
+}
+
+    bool isPalindrome(ListNode* head) {
+    // stack<int>s;
+    // ListNode* temp=head;
+    // while(temp!=NULL)
+    // {
+    //     s.push(temp->val);
+    //     temp=temp->next;
+    // }
+    // temp=head;
+    // while(temp!=NULL)
+    // {
+    //     if(temp->val!= s.top())
+    //      return false;
+    //    else
+    //    {
+    //        temp=temp->next;
+    //        s.pop();
+    //    }
+    // }
+    // return true;
+
+                                         //tortoise and hare then reverse the link & compare
+    if(head==NULL || head->next==NULL)
+        return true;
+    ListNode* slow=head;
+    ListNode* fast=head;
+    while(fast->next!=NULL && fast->next->next!=NULL)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    ListNode* newHead=reverse(slow->next);   // reverse the second half part
+    ListNode* first=head;
+    ListNode* second=newHead;
+    while(second!=NULL)
+    {
+        if(first->val!=second->val)
+        {
+            reverse(newHead);     //make sure before return, back to original data.
+            return false;
+        }
+        first=first->next;
+        second=second->next;
+    }
+    reverse(newHead);
+    return true;
+    }
+};
