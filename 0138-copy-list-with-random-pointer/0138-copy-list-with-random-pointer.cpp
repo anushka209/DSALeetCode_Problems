94f3266075ce48538913e9a21f6795fe8861799e
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-09-18 17:50:51 +0530

    Time: 3 ms (91.49%), Space: 12.6 MB (100%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -19,35 +19,35 @@ public:
     Node* copyRandomList(Node* head) {
         													//insert nodes in between
 	Node* temp=head;
-    while(temp!=NULL)
-    {
-        Node* copyNode=new Node(temp->val);
-        copyNode->next=temp->next;
-        temp->next=copyNode;
-        temp=temp->next->next;
-    }
+	while(temp!=NULL)
+	{
+		Node* copyNode=new Node(temp->val);
+		copyNode->next=temp->next;
+		temp->next=copyNode;
+		temp=temp->next->next;
+	}
 												// link random pointers
 	temp=head;
-    while(temp!=NULL)
-    {
-        Node* copyNode=temp->next;
-        if(temp->random)
-            copyNode->random=temp->random->next;
-        else
-            copyNode->random=NULL;
-        temp=temp->next->next;
-    }
+	while(temp!=NULL)
+	{
+		Node* copyNode=temp->next;
+		if(temp->random)
+			copyNode->random=temp->random->next;
+		else
+			copyNode->random=nullptr;
+		temp=temp->next->next;
+	}
 											//link next pointers
 	temp=head;
-    Node* dummy=new Node(-1);
-    Node* res=dummy;
-    while(temp!=NULL)
-    {
-        res->next=temp->next;
-        temp->next=temp->next->next;
-        res=res->next;
-        temp=temp->next;
-    }
-    return dummy->next;
+		Node* dummy=new Node(-1);
+		Node* res=dummy;
+	while(temp!=NULL)
+	{
+		res->next=temp->next;
+		temp->next=temp->next->next;
+		res=res->next;
+		temp=temp->next;
+	}
+	return dummy->next;
     }
 };
