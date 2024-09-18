21d780034ea430ece2321ad6f714228295fcdb37
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-09-18 13:40:05 +0530

    Time: 7 ms (43.82%), Space: 15.2 MB (99.96%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -10,37 +10,31 @@
  */
 class Solution {
 public:
-    ListNode* findNode(ListNode* temp,int k)
-    {
-        int count=1;
-        while(temp!=NULL)
-        {
-            if(count==k)
-                return temp;
-            count++;
-            temp=temp->next;
-        }
-        return temp;
-    }
-
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL || k==0)
-            return head;
-        ListNode* temp=head;
-        ListNode* tail=head;
-        int len=1;
-        while(tail->next!=NULL)
-        {
-            len++;
-            tail=tail->next;
-        }
-        if(k%len==0)
-            return head;
-        k=k%len;
-        tail->next=head;  //attach tail to the head
-        ListNode* newNode=findNode(head,len-k);
-        head=newNode->next;
-        newNode->next=NULL;
-        return head;
+    if(head==NULL || k==0)
+     return head;
+      ListNode* tail=head;
+      ListNode* temp=head;
+      int len=1;
+      while(tail->next!=NULL)
+     {
+          len++;
+          tail=tail->next;
+     }
+     if(k%len==0)
+          return head;
+     else{
+          k=k%len;
+       tail->next=head;
+     int num=len-k;
+     num--;
+     while (temp != NULL && num != 0) {
+       num--;
+       temp = temp->next;
+     }
+      head=temp->next;
+     temp->next = NULL;
+     }
+     return head;
     }
 };
