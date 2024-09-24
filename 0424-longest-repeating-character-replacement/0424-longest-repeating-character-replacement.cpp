02fa782a989f361b95bc075df24092bcbb7e8c57
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-09-24 17:22:17 +0530

    Time: 11 ms (45.77%), Space: 9.2 MB (44.4%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        int maxlen=0,l=0,r=0;
         int n=s.size();
-        int l=0,r=0,maxlen=0,maxf=0;
+        int maxf=0;
         map<char,int>mpp;
         while(r<n)
         {
             mpp[s[r]]++;
-            maxf=max(maxf,mpp[s[r]]);   // find maximum frequency
+            maxf=max(maxf,mpp[s[r]]);
             int len=r-l+1;
-            int conv=len-maxf;  // conversion needed
+            int conv=len-maxf;      //conversions needed
             if(conv>k)
             {
                 mpp[s[l]]--;
-                // maxf=0;                          //not needed to do becoz if maxF was decreased then maxlen cannot be increased..to get maxlen as more
-                // for(int i=0;i<mpp.size();i++)        // than previous we have to increase the maxF so not required to decrease or update maxF.
+                // maxf=0;
+                // for(int i=0;i<mpp.size();i++)
                 //     maxf=max(maxf,mpp[i].second);
                 l++;
             }
             if(conv<=k)
-            {
                 maxlen=max(maxlen,len);
-            }
             r++;
         }
         return maxlen;
