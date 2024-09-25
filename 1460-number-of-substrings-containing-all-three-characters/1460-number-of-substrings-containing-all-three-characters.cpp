e503ccdb4f3d0f3bb8f949fcc19d94f584f1150f
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-09-25 09:08:25 +0530

    Time: 19 ms (51.49%), Space: 10 MB (73.28%) - LeetHub

---
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+       int n=s.size(),count=0;
+       vector<int>hash(3,-1);
+       for(int i=0;i<n;i++)
+       {
+          hash[s[i]-'a']=i;
+          if(hash[0]!=-1 && hash[1]!=-1 && hash[2]!=-1)
+          {
+                count=count+1+min({hash[0],hash[1],hash[2]});
+          }
+       }
+       return count;
+    }
+};
