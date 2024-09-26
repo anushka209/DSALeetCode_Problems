496d0c1975d70f44fcdb679949ba47fcc7ded9e0
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-09-26 18:45:02 +0530

    Time: 4 ms (32.28%), Space: 10.2 MB (28.95%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,21 +9,43 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int>& res)
-    {
-        if(root==NULL)
-            return;
-        inorder(root->left,res);
-        res.push_back(root->val);
-        inorder(root->right,res);
-    }
+    // void inorder(TreeNode* root,vector<int>& res)
+    // {
+    //     if(root==NULL)
+    //         return;
+    //     inorder(root->left,res);
+    //     res.push_back(root->val);
+    //     inorder(root->right,res);
+    // }
 
     vector<int> inorderTraversal(TreeNode* root) {
        vector<int>res;
-       inorder(root,res);
-       return res;
+    //    inorder(root,res);
+    //    return res;
+
+     stack<TreeNode*>st;
+     TreeNode* node=root;
+     while(true)
+     {
+        if(node!=NULL)
+        {
+            st.push(node);
+            node=node->left;
+        }
+        else
+        {
+            if(st.empty())
+                break;
+            node=st.top();
+            st.pop();
+            res.push_back(node->val);
+            node=node->right;
+        }
+     }
+     return res;
  
 //  ------------------MORRIS TRAVERSAL----------------------------------------
         // vector<int>inorder;
