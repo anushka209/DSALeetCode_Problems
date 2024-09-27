52cc04c44981be886de41c13703c72d21d6c3e0b
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-09-27 17:38:26 +0530

    Time: 0 ms (100%), Space: 13.8 MB (99.95%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,18 +11,18 @@
  */
 class Solution {
 public:
-    void rev_preorder(TreeNode* root,int level,vector<int>& ans)
+    void rev_preorder(TreeNode* root,int level,vector<int> &ans)
     {
-        if(root==NULL)  return;             //we do reverse preorder ie. root right left
+        if(root==NULL)  return;
         if(level==ans.size())
             ans.push_back(root->val);
-        rev_preorder(root->right,level+1,ans);
+        rev_preorder(root->right,level+1,ans);    //we do reverse preorder ie. root right left
         rev_preorder(root->left,level+1,ans);
     }
 
     vector<int> rightSideView(TreeNode* root) {
-      vector<int>ans;
-      rev_preorder(root,0,ans);
-      return ans;
+        vector<int>ans;
+        rev_preorder(root,0,ans);
+        return ans;
     }
 };
