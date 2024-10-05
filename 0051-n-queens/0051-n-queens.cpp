2df532de2ece90dce8fef9165bea9f8e0ec2e5bc
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-05 12:11:27 +0530

    Time: 5 ms (57.35%), Space: 9.5 MB (76.28%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    void place_queen(int col,int n,vector<string> &board,vector<vector<string>> &ans,vector<int> &left_row,vector<int> &upper_diagonal,vector<int> &lower_diagonal)
+    void place_queen(int col,int n,vector<string> &board,vector<vector<string>> &ans,vector<int> &leftRow,vector<int> &lowerDiagonal,vector<int> &upperDiagonal)
     {
        if(col==n)
        {
           ans.push_back(board);
           return;
        }
-        for(int row=0;row<n;row++)      //iterate in every row
-        {
-            if(left_row[row]==0 && upper_diagonal[n-1+col-row]==0 && lower_diagonal[row+col]==0)
-            {
-                board[row][col]='Q';
-                left_row[row]=1;
-                upper_diagonal[n-1+col-row]=1;
-                lower_diagonal[row+col]=1;
-                place_queen(col+1,n,board,ans,left_row,upper_diagonal,lower_diagonal);
-                board[row][col]='.';   //backtrack
-                left_row[row]=0;
-                upper_diagonal[n-1+col-row]=0;
-                lower_diagonal[row+col]=0;
-            }
-        }
+       for(int row=0;row<n;row++)       //iterate in every row
+       {
+         if(leftRow[row]==0 && lowerDiagonal[row+col]==0 && upperDiagonal[n-1+col-row]==0)
+         {
+            board[row][col]='Q';
+            leftRow[row]=1;
+            lowerDiagonal[row+col]=1;
+            upperDiagonal[n-1+col-row]=1;
+            place_queen(col+1,n,board,ans,leftRow,lowerDiagonal,upperDiagonal);
+            board[row][col]='.';          //backtrack
+            leftRow[row]=0;
+            lowerDiagonal[row+col]=0;
+            upperDiagonal[n-1+col-row]=0;
+         }
+       }
     }
 
 public:
@@ -33,8 +33,8 @@ public:
       {
         board[i]=s;
       }
-      vector<int>left_row(n,0),lower_diagonal(2*n-1,0),upper_diagonal(2*n-1,0);
-      place_queen(0,n,board,ans,left_row,upper_diagonal,lower_diagonal);
+      vector<int>leftRow(n,0),lowerDiagonal(2*n-1,0),upperDiagonal(2*n-1,0);
+      place_queen(0,n,board,ans,leftRow,lowerDiagonal,upperDiagonal);
       return ans;
     }
 };
