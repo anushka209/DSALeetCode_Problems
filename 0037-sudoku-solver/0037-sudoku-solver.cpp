5b78a6357e5ff48854c25f062441774797db2043
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-05 16:26:14 +0530

    Time: 10 ms (81.24%), Space: 8 MB (33.02%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -4,8 +4,8 @@ public:
     {
        for(int i=0;i<9;i++)
        {
-        if(board[row][i]==c) return false;
-        if(board[i][col]==c) return false;
+        if(board[row][i]==c)    return false;
+        if(board[i][col]==c)    return false;
         if(board[3*(row/3)+ i/3][3*(col/3)+ i%3]==c) return false;
        }
        return true;
@@ -13,32 +13,31 @@ public:
 
     bool find(vector<vector<char>> &board)
     {
-      for(int i=0;i<board.size();i++)
-      {
+       for(int i=0;i<board.size();i++)
+       {
         for(int j=0;j<board[0].size();j++)
         {
             if(board[i][j]=='.')
             {
                 for(char ch='1';ch<='9';ch++)
                 {
-                    if(isvalid(i,j,ch,board))
-                    {
+                    if(isvalid(i,j,ch,board)==true)
+                     {
                         board[i][j]=ch;
-                        if(find(board))
+                        if(find(board)==true)
                             return true;
                         else
-                            board[i][j]='.';      //backtrack
-                    }
+                            board[i][j]='.';   //backtrack
+                     }
                 }
-                return false;       
+                return false;
             }
         }
-      }
-      return true;    //completely filled
+       }
+       return true;      //completely filled
     }
 
-    void solveSudoku(vector<vector<char>>& board) 
-    {
+    void solveSudoku(vector<vector<char>>& board) {
         find(board);
     }
 };
