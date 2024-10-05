9d7e849e18c09959388bb199624c8069fcef4010
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-05 13:32:52 +0530

    Time: 23 ms (18.25%), Space: 22.8 MB (10.02%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -2,34 +2,28 @@ class Solution {
 public:
     unordered_set<string>st;
     int dp[301];
-    bool find(int i,string &s)
+    bool solve(int idx,int n,string s,vector<string>& wordDict)
     {
-        if(i==s.size())
+        if(idx==n)
             return true;
-
-        if(dp[i]!=-1) return dp[i];
-
+        if(dp[idx]!=-1)  
+            return dp[idx];
         if(st.find(s)!=st.end())
             return true;
-        for(int idx=1;idx<=s.size();idx++)
+        for(int l=1;l<=n;l++)
         {
-            string temp=s.substr(i,idx);
-            if(st.find(temp)!=st.end())
-            {
-                if(find(i+idx,s))
-                    return dp[i]=true;
-            }
+            string temp=s.substr(idx,l);
+            if(st.find(temp)!=st.end() && solve(idx+l,n,s,wordDict))
+                return dp[idx]=true;
         }
-        return dp[i]=false;
+        return dp[idx]=false;
     }
 
     bool wordBreak(string s, vector<string>& wordDict) {
         int n=s.size();
-        // vector<bool>dp(n+1,false);
         memset(dp,-1,sizeof(dp));
-       for(string &word: wordDict)
+        for(string &word : wordDict)
             st.insert(word);
-        bool ans=find(0,s);
-        return ans;
+        return solve(0,n,s,wordDict);
     }
 };
