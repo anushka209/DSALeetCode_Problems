048140ebf3cb24f7d5d4c97cbdce61f53132682d
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-06 16:26:54 +0530

    Time: 0 ms (100%), Space: 8.7 MB (22.58%) - LeetHub

--- 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
@@ -2,34 +2,31 @@ class Solution {
 public:
     bool areSentencesSimilar(string sentence1, string sentence2)
     {
-    if(sentence1 == sentence2) return true;
+        if(sentence1.size()<sentence2.size())
+            swap(sentence1,sentence2);
+        vector<string>v1,v2;
 
-    vector<string> s1, s2;
-    stringstream ss1(sentence1),ss2(sentence2);
-    string word;
+        stringstream ss1(sentence1);
+        string token;
+        while(ss1>>token)
+            v1.push_back(token);
 
-    // Split sentence1 into words
-    while (ss1 >> word) s1.push_back(word);
+        stringstream ss2(sentence2);
+        while(ss2>>token)
+            v2.push_back(token);
 
-    // Split sentence2 into words
-    while (ss2 >> word) s2.push_back(word);
+        int i=0,j=v1.size()-1;  //sentence1 {v1}
+        int k=0,l=v2.size()-1;  //sentence2 {v2}
 
-    int s1si = 0, s1ei = s1.size() - 1;
-    int s2si = 0, s2ei = s2.size() - 1;
-
-    // Increment start indices while matching
-    while (s1si <= s1ei && s2si <= s2ei && s1[s1si] == s2[s2si]) {
-        s1si++;
-        s2si++;
-    }
-
-    // Decrement end indices while matching
-    while (s1ei >= s1si && s2ei >= s2si && s1[s1ei] == s2[s2ei]) {
-        s1ei--;
-        s2ei--;
-    }
-
-    return s1ei < s1si || s2ei < s2si;
-
+        while(i<v1.size() && k<v2.size() && v1[i]==v2[k])
+        {
+            i++; k++;
+        }
+        while(l>=k && v1[j]==v2[l])
+        {
+            j--; l--;
+        }
+        if(l<k) return true;
+        return false;
     }
 };
