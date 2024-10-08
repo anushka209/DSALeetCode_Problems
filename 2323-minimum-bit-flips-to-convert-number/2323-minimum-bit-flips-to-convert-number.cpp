19a6cad04e81725a77700f30ec8a5620c68cb091
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-08 10:09:22 +0530

    Time: 3 ms (40.67%), Space: 7.7 MB (41.81%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-    int C=start^goal;
-    int count=0;
-    for(int i=0;i<31;i++)
-    {
-        if(C & (1<<i))
-            count++;
-    }
-    return count;
+        int ans=start ^ goal;
+        int count=0;
+        for(int i=0;i<31;i++)
+        {
+            if(ans & (1<<i))   // check ith bit is set or not
+                count++;
+        }
+        return count;
     }
 };
