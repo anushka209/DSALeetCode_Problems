f0626fafa99f0ae56f015eab1885388dc9560552
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-08 13:40:02 +0530

    Time: 0 ms (100%), Space: 8.3 MB (42.39%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        int m=n;
-        n=abs(n);
-        double ans=1;
-        while(n>0)
-        { 
-            if(n%2==1)          // odd
-            {
-                ans=ans*x;
-                n=n-1;
-            }
-            else
-            {                  // even
-                n=n/2;
-                x=x*x;
-            }
-        }
-        if(m<0)
-            ans=1.0/ans;
+       int m=n;
+       n=abs(n);
+       double ans=1;
+       while(n>0)
+       {
+         if(n%2==1)      // if odd
+         {
+            ans=ans*x;
+            n=n-1;
+         }
+         else         // even
+         {
+            n=n/2;
+            x=x*x;
+         }
+       }
+       if(m<0)
+         ans=1.0/ans;
         return ans;
     }
 };
