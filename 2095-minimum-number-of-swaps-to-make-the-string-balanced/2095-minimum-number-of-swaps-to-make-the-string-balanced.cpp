bddfec41d7a2f0751236d81b0bfd1ae65e400961
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-08 12:05:27 +0530

    Time: 104 ms (58.75%), Space: 35.1 MB (45.63%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        int swapCount=0;
-        int imBalance=0;        // Tracks the imbalance between opening and closing brackets
-        for(char ch : s)
-        {
-            if(ch==']')
-            {
-                if(imBalance==0)   // No matching opening bracket
-                {
-                    swapCount++;
-                    imBalance++;      // Adjust imbalance to account for the swap
-                }
-                else
-                {
-                    imBalance--;         // A matching opening bracket is available, so balance out
-                }
-            }
-            else
-            {
-                imBalance++;          // Encountered an opening bracket, increase the imbalance
-            }
-        }
-        return swapCount;
+       stack<char>st;
+       for(int i=0;i<s.size();i++)
+       {
+         if(s[i]=='[')
+            st.push(s[i]);
+         else if(s[i]==']' && !st.empty())
+            st.pop();
+       }
+       return (st.size()+1)/2;
     }
 };
