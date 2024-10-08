cf6b30d990ee3ff32f9adf29f39714fa533dc720
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-08 12:10:18 +0530

    Time: 92 ms (92.62%), Space: 32.7 MB (79.33%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int minSwaps(string s) {
-       stack<char>st;
+       // stack<char>st;
+
+       int size=0;
        for(int i=0;i<s.size();i++)
        {
          if(s[i]=='[')
-            st.push(s[i]);
-         else if(s[i]==']' && !st.empty())
-            st.pop();
+            // st.push(s[i]);
+            size++;
+
+         //else if(s[i]==']' && !st.empty())
+           // st.pop();
+        else if(size>0)
+            size--;
        }
-       return (st.size()+1)/2;
+       // return (st.size()+1)/2;
+       return (size+1)/2;
     }
 };
