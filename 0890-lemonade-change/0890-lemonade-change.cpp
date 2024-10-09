48c2e561d13bde86776675c963e1a30182b28b03
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-09 11:27:29 +0530

    Time: 58 ms (93.02%), Space: 85.9 MB (83.46%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -4,30 +4,30 @@ public:
        int five=0,ten=0;
        for(int i=0;i<bills.size();i++)
        {
-          if(bills[i]==5)
+         if(bills[i]==5)
             five++;
-          else if(bills[i]==10)
-          {
+         if(bills[i]==10)
+         {
             if(five)
             {
-                five--;
                 ten++;
+                five--;
             }
             else
                 return false;
-          }
-          else
-          {
-             if(five && ten)
-             {
-                five--;
+         }
+         if(bills[i]==20)
+         {
+            if(ten && five)
+            {
                 ten--;
-             }
-             else if(five>=3)
+                five--;
+            }
+            else if(five>=3)
                 five-=3;
-             else
+            else 
                 return false;
-          }
+         }
        }
        return true;
     }
