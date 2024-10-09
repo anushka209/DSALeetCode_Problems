bb9bf65925b4b985a0afaadca45d2067ab6d7617
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-09 08:47:55 +0530

    Time: 0 ms (100%), Space: 7.6 MB (99.04%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        int close=0;
-        stack<int>st;
+        int open=0,close=0;
+        // stack<int>st;
         for(int i=0;i<s.size();i++)
         {
             if(s[i]=='(')
-                st.push(s[i]);
-            else if(s[i]==')' && !st.empty())
-                st.pop();
+               // st.push(s[i]);
+               open++;
+           // else if(s[i]==')' && !st.empty())
+               // st.pop();
+            else if(s[i]==')' && open>0)
+               open--;
             else
                 close++;
         }
-        return st.size()+close;
+      //  return st.size()+close;
+      return open+close;
     }
 };
