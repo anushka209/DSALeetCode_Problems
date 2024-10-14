9f8d0cf579ffe61a29fef8269e5ac61551fd24dd
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-14 17:20:26 +0530

    Time: 198 ms (47.88%), Space: 79.9 MB (21.7%) - LeetHub

---
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+    // Add all elements to the priority queue
+    for(int num : nums)
+    {
+        pq.push(num);
+    }
+    long long score = 0;
+    while (k > 0)
+    {
+        int ele = pq.top();
+        pq.pop();
+        score += ele;
+        pq.push(ceil(ele / 3.0));
+        k--;
+    }
+    return score;
+    }
+};
