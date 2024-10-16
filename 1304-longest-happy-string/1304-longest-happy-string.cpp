03a5aace4e27c5674f373f3ac22188eac161bb54
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-16 16:26:57 +0530

    Time: 0 ms (100%), Space: 7.8 MB (64.09%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.cpp
+++ 1304-longest-happy-string/1304-longest-happy-string.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
-        priority_queue<pair<int, char>> pq;
-        if (a > 0) pq.push({a, 'a'});
-        if (b > 0) pq.push({b, 'b'});
-        if (c > 0) pq.push({c, 'c'});
-        string result = "";
-        while (!pq.empty()) {
-            // Get the character with the most count.
-            auto [count1, char1] = pq.top();
+        priority_queue<pair<int,char>>pq;
+        if(a>0)  pq.push({a,'a'});
+        if(b>0)  pq.push({b,'b'});
+        if(c>0)  pq.push({c,'c'});
+        string ans="";
+        while(!pq.empty())
+        {
+            int currCount=pq.top().first;
+            int currChar=pq.top().second;
             pq.pop();
-
-            // Check if the last two characters in result are the same.
-            if (result.size() >= 2 && result.back() == char1 && result[result.size() - 2] == char1) {
-                if (pq.empty()) break;  // No valid characters left.
-
-                // Get the second most character.
-                auto [count2, char2] = pq.top();
+            if(ans.size()>=2 && currChar==ans[ans.size()-1] && currChar==ans[ans.size()-2])   // cannot use currChar check for next largest char
+            {
+                if(pq.empty())
+                    break;
+                int nextCount=pq.top().first;
+                int nextChar=pq.top().second;
                 pq.pop();
-
-                // Add the second character to avoid consecutive repetition.
-                result += char2;
-                count2--;
-
-                if (count2 > 0) pq.push({count2, char2});
-
-                // Push back the most frequent character for later use.
-                pq.push({count1, char1});
-            } else {
-                // If no repetition issue, add the most frequent character.
-                result += char1;
-                count1--;
-
-                if (count1 > 0) pq.push({count1, char1});
+                ans.push_back(nextChar);
+                nextCount--;
+                if(nextCount>0)
+                    pq.push({nextCount,nextChar});
             }
+            else
+            {
+                ans.push_back(currChar);
+                currCount--;
+            }
+            if(currCount>0)
+              pq.push({currCount,currChar});
         }
-        return result;
+        return ans;
     }
 };
