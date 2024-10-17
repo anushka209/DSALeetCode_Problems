f00743bc12fb91987d3af558dfeccdb5edd68e33
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-17 17:09:37 +0530

    Time: 4 ms (84.39%), Space: 17 MB (65.54%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    void dfs(int r,int c,vector<vector<int>>& image,vector<vector<int>>& ans,int color,int dr[],int dc[],int iniCol)
+    void dfs(int sr,int sc,vector<vector<int>>& image,int color,int dr[],int dc[],vector<vector<int>>& ans,int iniCol)
     {
-        ans[r][c]=color;
-        int n=image.size();
-        int m=image[0].size();
-        for(int i=0;i<4;i++)     //becoz we have to move in 4 directions
+        ans[sr][sc]=color;
+        int m=image.size();
+        int n=image[0].size();
+        for(int i=0;i<4;i++)        //becoz we have to move in 4 directions
         {
-            int row=r+dr[i];
-            int col=c+dc[i];
-            if(row>=0 && row<n && col>=0 && col<m && image[row][col]==iniCol && ans[row][col]!=color)
-                dfs(row,col,image,ans,color,dr,dc,iniCol);
+            int newr=sr+dr[i];
+            int newc=sc+dc[i];
+            if(newr>=0 && newr<m && newc>=0 && newc<n && image[newr][newc]==iniCol && ans[newr][newc]!=color)
+                dfs(newr,newc,image,color,dr,dc,ans,iniCol);
         }
     }
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-       int iniCol=image[sr][sc];
-       vector<vector<int>>ans=image;  //copy the data given
-       int dr[]={-1,0,+1,0};
-       int dc[]={0,+1,0,-1};
-       dfs(sr,sc,image,ans,color,dr,dc,iniCol);
-       return ans;
+        int iniCol=image[sr][sc];
+        vector<vector<int>>ans=image;        //copy the data given
+        int dr[]={-1,0,+1,0};
+        int dc[]={0,+1,0,-1};
+        dfs(sr,sc,image,color,dr,dc,ans,iniCol);
+        return ans;
     }
 };
