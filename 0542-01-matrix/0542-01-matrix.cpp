5abb5ec9733144d358415083ffe9bc5d5cb27cc4
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-17 19:22:39 +0530

    Time: 22 ms (99.99%), Space: 38.3 MB (33.83%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -5,15 +5,15 @@ public:
         int n=mat[0].size();
         vector<vector<int>>vis(m,vector<int>(n,0));
         vector<vector<int>>dist(m,vector<int>(n,0));
-        queue<pair<pair<int,int>,int>>q;         //row,col,distance
+        queue<pair<pair<int,int>,int>>q;          // row,col,distance
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(mat[i][j]==0)
                 {
-                    vis[i][j]=1;
                     q.push({{i,j},0});
+                    vis[i][j]=1;
                 }
                 else
                     vis[i][j]=0;
@@ -21,21 +21,21 @@ public:
         }
         int dr[]={-1,0,+1,0};
         int dc[]={0,+1,0,-1};
-        while(!q.empty())     //using bfs
+        while(!q.empty())    // using bfs 
         {
             int row=q.front().first.first;
             int col=q.front().first.second;
-            int distance=q.front().second;
+            int dis=q.front().second;
             q.pop();
-            dist[row][col]=distance;
+            dist[row][col]=dis;
             for(int i=0;i<4;i++)
             {
                 int newr=row+dr[i];
                 int newc=col+dc[i];
                 if(newr>=0 && newr<m && newc>=0 && newc<n && vis[newr][newc]==0)
                 {
+                    q.push({{newr,newc},dis+1});
                     vis[newr][newc]=1;
-                    q.push({{newr,newc},distance+1});
                 }
             }
         }
