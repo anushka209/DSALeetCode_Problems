42d5e16222834de121844c4f6e9e5f6bded7b861
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-17 12:48:23 +0530

    Time: 20 ms (66.85%), Space: 19.7 MB (34.16%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void dfs(int node,vector<int>& vis,vector<int> adj[])
+    void dfs(int node,vector<int> adj[],vector<int>& vis)
     {
-      vis[node]=1;
-      for(auto it: adj[node])
-      {
-        if(!vis[it])
-            dfs(it,vis,adj);
-      }
+        vis[node]=1;
+        for(auto it: adj[node])
+        {
+            if(!vis[it])
+                dfs(it,adj,vis);
+        }
     }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
-       int n=isConnected.size();
-       vector<int>adj[n];          //convert from adjacency matrix to adjacency list
-       for(int i=0;i<n;i++)
-       {
+      int n=isConnected.size();
+      vector<int>adj[n];        //convert from adjacency matrix to adjacency list
+      for(int i=0;i<n;i++)
+      {
         for(int j=0;j<n;j++)
         {
             if(isConnected[i][j]==1 && i!=j)
@@ -23,17 +23,17 @@ public:
                 adj[j].push_back(i);
             }
         }
-       }
-       vector<int>vis(n,0);
-       int count=0;
-       for(int i=0;i<n;i++)
-       {
+      }
+      vector<int>vis(n,0);
+      int count=0;
+      for(int i=0;i<n;i++)
+      {
         if(!vis[i])
         {
             count++;
-            dfs(i,vis,adj);
+            dfs(i,adj,vis);
         }
-       }
-       return count;
+      }
+      return count;
     }
 };
