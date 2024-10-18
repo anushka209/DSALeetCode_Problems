049282e0a0b2f0793ffb9353d2bfe24ead911f56
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-18 18:56:34 +0530

    Time: 0 ms (100%), Space: 16.4 MB (76.31%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,61 +1,63 @@
 class Solution {
 public:
-    // bool dfs(int node,int col,vector<int>& color,vector<vector<int>>& graph)
-    // {
-    //     color[node]=col;
-    //     for(auto it: graph[node]){
-    //         if(color[it]==-1){
-    //             if(dfs(it,!col,color,graph)==false)
-    //                 return false;
-    //         }
-    //         else if(color[it]==color[node])
-    //             return false;
-    //     }
-    //     return true;
-    // }
+    bool dfs(int node,int col,vector<int>& color,vector<vector<int>>& graph)
+    {
+       color[node]=col;
+       for(auto it : graph[node])
+       {
+         if(color[it]==-1)
+         {
+            if(dfs(it,!col,color,graph)==false)
+                return false;
+         }
+         else if(color[it]==color[node])
+            return false;
+       }
+       return true;
+    }
 
     bool isBipartite(vector<vector<int>>& graph) 
     {
-        int n=graph.size();   // by BFS
-        vector<int> color(n,-1);
-        for(int i=0;i<n;i++)        // Check each component of the graph
-        {
-           if(color[i]==-1)
-           {
-              queue<int>q;
-              q.push(i);
-              color[i]=0;
-
-              while(!q.empty())
-              {
-                int node=q.front();
-                q.pop();
-                for(auto it : graph[node])
-                {
-                    if(color[it]==-1)          //if the adjacent node is yet not colored, give opposite color to the node
-                    {
-                        color[it]=!color[node];
-                        q.push(it);
-                    }
-                 //if the adjacent having the same color not a bipartite graph
-                    else if(color[it]==color[node])
-                        return false;
-                    }
-                }
-              }
-           }
-        return true;
-
-        //  int n=graph.size(); 
-        // vector<int>color(n,-1);         //by DFS
-
-        // for(int i=0;i<n;i++)      //for components
+        // int n=graph.size();   // by BFS
+        // vector<int> color(n,-1);
+        // for(int i=0;i<n;i++)        // Check each component of the graph
         // {
-        //     if(color[i]==-1){
-        //         if(dfs(i,0,color,graph)==false)
-        //             return false;
-        //     }
-        // }
+        //    if(color[i]==-1)
+        //    {
+        //       queue<int>q;
+        //       q.push(i);
+        //       color[i]=0;
+
+        //       while(!q.empty())
+        //       {
+        //         int node=q.front();
+        //         q.pop();
+        //         for(auto it : graph[node])
+        //         {
+        //             if(color[it]==-1)          //if the adjacent node is yet not colored, give opposite color to the node
+        //             {
+        //                 color[it]=!color[node];
+        //                 q.push(it);
+        //             }
+        //          //if the adjacent having the same color not a bipartite graph
+        //             else if(color[it]==color[node])
+        //                 return false;
+        //             }
+        //         }
+        //       }
+        //    }
         // return true;
+
+       int n=graph.size();
+       vector<int>color(n,-1);   // dfs
+       for(int i=0;i<n;i++)       //for components
+       {
+         if(color[i]==-1)
+         {
+            if(dfs(i,0,color,graph)==false)
+                return false;
+         }
+       }
+       return true;
     }
 };
