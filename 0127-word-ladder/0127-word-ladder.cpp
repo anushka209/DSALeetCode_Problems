998c07970856d7475d5cd95147f8494eba3b9cde
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-18 14:06:26 +0530

    Time: 55 ms (94.63%), Space: 19.4 MB (87.25%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        queue<pair<string,int>>q;       //by bfs ie. level-wise traversal
-        q.push({beginWord,1});
+      queue<pair<string,int>> q;   //bfs - level order traversal
+// Push all values of wordList into a set to make deletion from it easier and also finding word is easier and in less time complexity ie. amotised. 
+      unordered_set<string>st(wordList.begin(),wordList.end());
+      q.push({beginWord,1});
+      st.erase(beginWord);
+      while(!q.empty())
+      {
+         string word=q.front().first;
+         int step=q.front().second;
+         q.pop();
+         if(word==endWord)  return step;     // return answer
 
-    // Push all values of wordList into a set to make deletion from it easier and also finding word is easier and in less time complexity ie. amotised.
-        unordered_set<string>st(wordList.begin(),wordList.end());    
-        st.erase(beginWord);
-
-        while(!q.empty())
-        {
-            string word=q.front().first;
-            int len=q.front().second;
-            q.pop();
-
-            if(word==endWord)  return len;    //return answer
-
-            for(int i=0;i<word.size();i++)
+         for(int i=0;i<word.size();i++)
+         {     // Now, replace each character of ‘word’ with char from a-z then check if ‘word’ exists in wordList.
+            char original=word[i];
+            for(int ch='a';ch<='z';ch++)
             {
-         // Now, replace each character of ‘word’ with char from a-z then check if ‘word’ exists in wordList.
-                char orginal=word[i];
-                for(char ch='a';ch<='z';ch++)
+                word[i]=ch;
+                if(st.find(word)!=st.end())
                 {
-                    word[i]=ch;
-                    if(st.find(word)!=st.end())
-                    {
-                        st.erase(word);
-                        q.push({word,len+1});  //increase the length
-                    }
+                    q.push({word,step+1});      //increase the length
+                    st.erase(word);
                 }
-                word[i]=orginal;    //for next conversions bring back to original
             }
-        }
-        return 0;
+            word[i]=original;   //for next conversions bring back to original
+         }
+      }
+      return 0;
     }
 };
