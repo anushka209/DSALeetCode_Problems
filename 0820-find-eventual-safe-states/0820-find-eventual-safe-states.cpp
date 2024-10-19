20bcd5835c58ed579969735ae65439b3f463c3f6
Anushka Gupta <112374486+anushka209@users.noreply.github.com>
2024-10-19 14:11:12 +0530

    Time: 49 ms (100%), Space: 62.8 MB (44.61%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int>adjRev[n];
-        vector<int>indegree(n,0);
-
-        for(int i=0;i<n;i++)
+      int n=graph.size();
+      vector<int> RevAdj[n];
+      vector<int>indegree(n,0);
+      for(int i=0;i<n;i++)
+      {
+         for(auto it:graph[i])
+         {
+            RevAdj[it].push_back(i);   // it->i reverse the edges to solve it using indegree concept
+            indegree[i]++;
+         }
+      }
+      queue<int>q;
+      for(int i=0;i<n;i++)
+      {
+        if(indegree[i]==0)
+            q.push(i);
+      }
+      vector<int>safe;
+      while(!q.empty())
+      {
+        int node=q.front();
+        q.pop();
+        safe.push_back(node);
+        for(auto it:RevAdj[node])
         {
-            for(auto it:graph[i])
-            {
-                adjRev[it].push_back(i);
-                indegree[i]++;
-            }
+            indegree[it]--;
+            if(indegree[it]==0)
+                q.push(it);
         }
-        queue<int>q;
-        for(int i=0;i<n;i++)
-        {
-            if(indegree[i]==0)
-                q.push(i);
-        }
-        vector<int>topo;
-        while(!q.empty())
-        {
-            int node=q.front();
-            q.pop();
-            topo.push_back(node);
-            for(auto it:adjRev[node])
-            {
-                indegree[it]--;
-                if(indegree[it]==0)
-                    q.push(it);
-            }
-        }
-        sort(topo.begin(),topo.end());
-        return topo;
+      }
+      sort(safe.begin(),safe.end());
+      return safe;
     }
 };
